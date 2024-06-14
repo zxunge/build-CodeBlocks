f99c5312a2c3a257a1fa52ff58f720d8cf5e556c
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-14 20:27:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 set -eux
 
 pacman -Syy
-pacman -S --noconfirm  wget git mingw-w64-i686-toolchain
+pacman -S --noconfirm  wget git
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2
-tar -jxvf ./wxWidgets-3.2.5.tar.bz2
+tar -jxf ./wxWidgets-3.2.5.tar.bz2
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 cp -f ./setup.h ./wxWidgets-3.2.5/include/wx/msw/
 cp -f ./config.gcc ./wxWidgets-3.2.5/build/msw/
-cd ./wxWidgets-3.2.5/build/msw
-cmd /c make -f makefile.gcc setup_h
-cmd /c make -f makeflie.gcc -j8
+cmd
+cd wxWidgets-3.2.5/build/msw
+D:/a/_temp/setup-msys2/mingw32/bin/make -f makefile.gcc setup_h
+D:/a/_temp/setup-msys2/mingw32/bin/makemake -f makeflie.gcc -j8
