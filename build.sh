4bfe2f754e48de8303eb9b503b5b7bcf8d60da67
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-14 19:44:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -eux
 
 pacman -Syy
 pacman -S --noconfirm  wget
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2
+tar -jxvf ./wxWidgets-3.2.5.tar.bz2
+cd wxWidgets-3.2.5
