47c23ac1fb730b18a1f8064e6e6d08506ed64076
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-15 18:43:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,27 @@ WXMSW_VERSION=3.2.5
 CBREV_NO=13529
 NAME=CodeBlocks-r${CBREV_NO}-wxWidgets-${WXMSW_VERSION}
 HOME=$(cygpath -m /home)
+MINGW32=$(cygpath -m /mingw32)
 
+# Install dependencies
 pacman -Syy
 pacman -S --noconfirm  wget git mingw-w64-i686-toolchain p7zip
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2
-tar -jxf ./wxWidgets-3.2.5.tar.bz2
+
+# Building at /home for convenience
+cd /home
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v${WXMSW_VERSION}/wxWidgets-${WXMSW_VERSION}.tar.bz2
+tar -jxf ./wxWidgets-${WXMSW_VERSION}.tar.bz2
+
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 ./cbp2make/cbp2make -in ./codeblocks_sfmirror/src/CodeBlocks_wx32.cbp -out ./codeblocks_sfmirror/src/makefile-core
-cat ./codeblocks_sfmirror/src/makefile-core
+
+# Build wxWidgets
+cp -f ./setup.h ./wxWidgets-${WXMSW_VERSION}/include/wx/msw/
+cp -f ./config.gcc ./wxWidgets-${WXMSW_VERSION}/build/msw/
+cd wxWidgets-${WXMSW_VERSION}/build/msw
+mingw32-make -f makefile.gcc setup_h
+mingw32-make -f makefile.gcc -j$(nproc)
+
+# We return to cmd to build CodeBlocks, pass some paths
+echo "HOME=${HOME}" >> $GITHUB_OUTPUT
+echo "MINGW32=${MINGW32}" >> $GITHUB_OUTPUT
