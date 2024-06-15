8937ba7edba1ff9086c8b64d924e28b714801be2
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-15 11:09:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ CBREVNO=13529
 NAME=CodeBlocks-r${CBREVNO}-wxWidgets-${WXMSW_VERSION}
 
 pacman -Syy
-pacman -S --noconfirm  wget git mingw-w64-i686-toolchain
+pacman -S --noconfirm  wget git mingw-w64-i686-toolchain p7zip
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2
 tar -jxf ./wxWidgets-3.2.5.tar.bz2
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
@@ -16,6 +16,9 @@ cp -f ./config.gcc ./wxWidgets-3.2.5/build/msw/
 cd wxWidgets-3.2.5/build/msw
 mingw32-make -f makefile.gcc setup_h
 mingw32-make -f makefile.gcc -j8
+
+7zr a -mx9 -mqs=on -mmt=on ${NAME}.7z ../../lib
+
 if [[ -v GITHUB_WORKFLOW ]]; then
   echo "::set-output name=OUTPUT_BINARY::${NAME}.7z"
   echo "::set-output name=RELEASE_NAME::CodeBlocks-r${CBREVNO}-wxWidgets-${WXMSW_VERSION}"
