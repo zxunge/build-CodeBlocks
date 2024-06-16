092037c76f4228f4f422a55f7906648b344ec76f
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-16 11:13:22 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ CBREV_NO=13529
 NAME=CodeBlocks-r${CBREV_NO}-wxWidgets-${WXMSW_VERSION}
 HOME=$(cygpath -m /home)
 
-7zr a -mx9 -mqs=on -mmt=on /home/${NAME}.7z /home/binaries
+7zr a -mx9 -mqs=on -mmt=on /home/${NAME}.7z /home/codeblocks_sfmirror/src/output32
 
 if [[ -v GITHUB_WORKFLOW ]]; then
   echo "OUTPUT_BINARY=${HOME}/${NAME}.7z" >> $GITHUB_OUTPUT
