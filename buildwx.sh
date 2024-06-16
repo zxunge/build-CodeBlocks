6d182ab4aa8e1176808b474f567b9d0a1ff48da9
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-16 11:06:36 +0800

    Update buildwx.sh

--- buildwx.sh
+++ buildwx.sh
@@ -12,16 +12,23 @@ MINGW32=$(cygpath -m /mingw32/bin)
 pacman -Syy
 pacman -S --noconfirm  wget git mingw-w64-i686-toolchain p7zip
 
-cp -r * /home/
-
 # Building at /home for convenience
+cp -r * /home/
 cd /home
 
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v${WXMSW_VERSION}/wxWidgets-${WXMSW_VERSION}.tar.bz2
+tar -jxf ./wxWidgets-${WXMSW_VERSION}.tar.bz2
 
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 ./cbp2make/cbp2make -in ./codeblocks_sfmirror/src/CodeBlocks_wx32.cbp -out ./codeblocks_sfmirror/src/makefile-core
 cat ./codeblocks_sfmirror/src/makefile-core
 
+# Build wxWidgets
+cp -f ./setup.h ./wxWidgets-${WXMSW_VERSION}/include/wx/msw/
+cp -f ./config.gcc ./wxWidgets-${WXMSW_VERSION}/build/msw/
+cd wxWidgets-${WXMSW_VERSION}/build/msw
+mingw32-make -f makefile.gcc setup_h
+mingw32-make -f makefile.gcc -j$(nproc)
 
 # We return to cmd to build CodeBlocks, pass some paths
 echo "HOME=${HOME}" >> $GITHUB_OUTPUT
