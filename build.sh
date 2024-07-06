6e37ce7042d80cb06fb1c5cac5ee36482c017fb1
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-07-06 11:09:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,51 +4,25 @@ set -eux
 
 CBREV_NO=13536
 WXMSW_VERSION=3.2.5
-GCC_LINK=https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
-WXMSW_LINK=https://github.com/zxunge/build-wxWidgets/releases/download/wxWidgets-3.2.5-14-stl-cb/wxWidgets-3.2.5-14-stl-cb.7z
-PRECB_LINK=https://wxstuff.xaviou.fr/download/codeblocks/windows/CB_20240626_rev13533_wx32_win32.7z
-WXINCLUDE_LINK=https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5-headers.7z
 HOME_PATH=$(cygpath -m ~)
 RELEASE_NAME=CodeBlocks-r${CBREV_NO}-wx${WXMSW_VERSION}
 
-wget -q ${GCC_LINK}
-wget -q ${WXMSW_LINK}
-wget -q ${PRECB_LINK}
-wget -q ${WXINCLUDE_LINK}
-7z x winlibs-*.7z -o./compilergcc/
-7z x wxWidgets-*.7z -o./wxwin/
-7z x CB_*.7z -o./precb/
-
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 
-BUILD_TYPE=--build
-START_CMD='start "Code::Blocks Build (wx3.2.x)" /D"%~dp0" /min /b'
-# ------------------------------------------------
-# Setup C::B application to run the *binaries* (!)
-# ------------------------------------------------
-export CB_ROOT=$HOME/precb
-export GCC_ROOT=$HOME/compilergcc/mingw32
-export PATH=$HOME/precb/:$HOME/compilergcc/mingw32/bin/:$PATH
-# ------------------------------------------------
-# Setup folder to the C::B *sources* from SVN (!)
-# ------------------------------------------------
-export CB_SRC=$HOME/codeblocks_sfmirror/src
+cd codeblocks_sfmirror/
+./bootstrap
+./configure --prefix=$HOME/codeblocks --with-contrib-plugins=all,-NassiShneiderman
 
-# -------------------------------------------
-# Usually below here no changes are required.
-# -------------------------------------------
-if [ -e $CB_APP ]; then
-  if [ -d $CB_SRC ]; then
-    export CB_PARAMS="--batch-build-notify --no-batch-window-close"
-    export CB_TARGET="--target=All"
-    export CB_CMD="--build $CB_SRC/CodeBlocks-wx32.workspace"
+make -j$(nproc)
+make install
 
-    echo $CB_APP $CB_PARAMS $CB_TARGET $CB_CMD
-    $CB_APP $CB_PARAMS $CB_TARGET $CB_CMD
-    echo "Do not forget to run 'update' after successful build!"
-  else
-    echo "C::B sources at '$CB_SRC' not found."
-  fi
-else
-  echo "File '$CB_APP' does not exists."
+7zr a -mx9 -mqs=on -mmt=on $HOME/${RELEASE_NAME}.7z $HOME/codeblocks
+
+if [[ -v GITHUB_WORKFLOW ]]; then
+  echo "OUTPUT_BINARY=${HOME_PATH}/${RELEASE_NAME}.7z" >> $GITHUB_OUTPUT
+  echo "RELEASE_NAME=${RELEASE_NAME}" >> $GITHUB_OUTPUT
+  echo "WXMSW_VERSION=${WXMSW_VERSION}" >> $GITHUB_OUTPUT
+  echo "CBREV_NO=${CBREV_NO}" >> $GITHUB_OUTPUT
+  echo "OUTPUT_NAME=${RELEASE_NAME}.7z" >> $GITHUB_OUTPUT
 fi
+
