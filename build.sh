b15fe6f511fd203f5fc0b381c95d3d03cec92c28
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-07-07 16:05:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ RELEASE_NAME=CodeBlocks-r${CBREV_NO}-wx${WXMSW_VERSION}
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 
 cd codeblocks_sfmirror/
-git apply -v ../002-fix-32bit-build.patch
+cp -f ../Makefile.am src/src/
 ./bootstrap
 ./configure --prefix=$HOME/codeblocks --with-contrib-plugins=all,-NassiShneiderman
 
